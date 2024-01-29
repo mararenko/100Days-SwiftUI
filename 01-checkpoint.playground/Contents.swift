adfde2bed34e1f4c6c13cd564d8e92b33b71270f
mararenko <nastja.mararenko@gmail.com>
2024-01-29 15:42:37 +0200

    02-checkpoint

--- 01-checkpoint.playground/Contents.swift
+++ 01-checkpoint.playground/Contents.swift
@@ -1,5 +1,3 @@
-import Cocoa
-
 let celsius = 25.0
 let fahrenheit = celsius * 9 / 5 + 32
 print("\(celsius)°C = \(fahrenheit)°F")
---
+++ 02-checkpoint.playground/Contents.swift
@@ -0,0 +1,4 @@
+let albums = ["Red", "Fearless", "1989", "Adele", "Red"]
+let newAlbums = Set(albums)
+
+print("Total albums: \(albums.count), new albums: \(newAlbums.count)")
